d102fe5f49eda8be6162809185bfc83927dc3fb8
Tumani1337 <idi_suda06@mail.ru>
2025-08-18 16:57:47 +0300

    написал инициализацию бота

---
+++ config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+
+BOT_TOKEN = ""
+
+DB_PATH = Path("database.db")
+
+WORKOUT_TYPES = [
+    "бег",
+    "ходьба",
+    "велосипед",
+    "плавание",
+    "тренажёрный зал",
+    "другое"
+]
+
+STAT_PERIODS = [
+    "день",
+    "неделя",
+    "месяц",
+    "год"
+]
---
+++ main.py
@@ -0,0 +1,13 @@
+import asyncio
+
+from aiogram import Bot,Dispatcher
+from config import BOT_TOKEN
+
+async def main():
+    bot = Bot(BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_routers()
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
