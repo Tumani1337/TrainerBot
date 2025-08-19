35a41f0c681651c822f7c513e3e2a30f5ba947cf
Tumani1337 <idi_suda06@mail.ru>
2025-08-19 14:45:45 +0300

    добавил в класс UserRepo метод add_user

--- repo.py
+++ repo.py
@@ -19,3 +19,20 @@ class UserRepo:
                 )
             """)
             await db.commit()
+
+    async def add_user(self, telegram_id: int, name: str) -> User:
+        registration_date = datetime.now().isoformat()
+        async with aiosqlite.connect(self.db_path) as db:
+            cursor = await db.execute("""
+                INSERT INTO users (telegram_id, name, registration_date, last_activity)
+                VALUES (?, ?, ?, ?)
+            """, (telegram_id, name, registration_date, registration_date))
+            await db.commit()
+            return User(
+                id=cursor.lastrowid,
+                telegram_id=telegram_id,
+                name=name,
+                registration_date=datetime.fromisoformat(registration_date),
+                last_activity=datetime.fromisoformat(registration_date)
+            )
+        
