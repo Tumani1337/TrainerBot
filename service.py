40463bde8be39214ae9903132e110f83480b6cb6
Tumani1337 <idi_suda06@mail.ru>
2025-08-20 17:05:57 +0300

    добавил класс UserService и метод register_user в service

--- service.py
+++ service.py
@@ -4,3 +4,12 @@ from models import User, Workout, Goal, Reminder
 from repo import UserRepo, WorkoutRepo, GoalRepo, ReminderRepo
 import config
 
+class UserService:
+    def __init__(self, user_repo: UserRepo):
+        self.user_repo = user_repo
+
+    async def register_user(self, telegram_id: int, name: str) -> User:
+        existing_user = await self.user_repo.get_user_by_telegram_id(telegram_id)
+        if existing_user:
+            return existing_user
+        return await self.user_repo.add_user(telegram_id, name)
