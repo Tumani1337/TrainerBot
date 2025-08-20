61cb31cdce2c7babc0fa529dc2e0ef254fdd7c45
Tumani1337 <idi_suda06@mail.ru>
2025-08-20 17:04:35 +0300

    добавил импорты в service

---
+++ service.py
@@ -0,0 +1,6 @@
+from datetime import datetime, timedelta
+from typing import List, Optional
+from models import User, Workout, Goal, Reminder
+from repo import UserRepo, WorkoutRepo, GoalRepo, ReminderRepo
+import config
+
