98c65d20460b79da2255f5608b0c9d043bedda4e
Tumani1337 <idi_suda06@mail.ru>
2025-08-22 16:35:56 +0300

    добавил импоты в keyboards

---
+++ keyboards.py
@@ -0,0 +1,3 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
+from config import WORKOUT_TYPES, STAT_PERIODS
