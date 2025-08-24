fe956eda44cc0ce4f437b3b5c9f358db16d76eba
Tumani1337 <idi_suda06@mail.ru>
2025-08-24 15:52:48 +0300

    добавил импорты в workaouts

---
+++ handlers/workouts.py
@@ -0,0 +1,6 @@
+from aiogram import Router, F
+from aiogram.filters import Command, StateFilter
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
