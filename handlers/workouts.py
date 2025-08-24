1f91ea443d347b582d4e14bb7cd81a6c2d61bf3c
Tumani1337 <idi_suda06@mail.ru>
2025-08-24 15:57:57 +0300

    добавил импорты в workouts

--- handlers/workouts.py
+++ handlers/workouts.py
@@ -4,3 +4,13 @@ from aiogram.types import Message, CallbackQuery
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 
+from keyboards import (
+    workout_types,
+    periods_keyboard,
+    confirm_cancel,
+    back_button
+)
+from service import WorkoutService, UserService
+from models import Workout
+from datetime import datetime
+import re
