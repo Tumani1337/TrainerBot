1d94e47cac8fdc833242c244a1960927cb20d773
Tumani1337 <idi_suda06@mail.ru>
2025-09-11 13:26:04 +0300

    добавил хэндлер на команду /statistics и кнопку Статистика

--- handlers/stats.py
+++ handlers/stats.py
@@ -11,3 +11,13 @@ from keyboards import (
 from service import WorkoutService, UserService, StatsService
 
 router = Router()
+
+@router.message(F.text.in_(("📊 Статистика", "/statistics")))
+@router.message(Command("statistics"))
+async def statistics_menu(message: Message):
+    await message.answer(
+        "Выберите период для статистики:",
+        reply_markup=periods_keyboard()
+    )
+
+    
